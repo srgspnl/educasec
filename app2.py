1245f81ff2ccf019c9c84a1d3fa775721ee47643
Sergio Spinola <srgspnl@gmail.com>
2025-09-04 18:44:31 -0300

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,3 +1,5 @@
+# app.py
+
 import streamlit as st
 
 # Importa as funções que contêm as lógicas de cada cifra
@@ -6,7 +8,6 @@ from vigenere import app as vigenere_app
 from enigma import app as enigma_app
 from rsa import app as rsa_app
 from hash import app as hash_app
-from nuvem import app as nuvem_app
 
 # Configuração da página principal
 st.set_page_config(
@@ -18,7 +19,7 @@ st.set_page_config(
 st.sidebar.title("Navegação")
 opcao_selecionada = st.sidebar.radio(
     "Escolha a cifra:",
-    ("Cifra de César", "Cifra de Vigenère", "Enigma", "RSA", "Hash", "Nuvem")
+    ("Cifra de César", "Cifra de Vigenère", "Enigma", "RSA", "Hash")
 )
 
 # Lógica para exibir a página correta
@@ -32,8 +33,6 @@ elif opcao_selecionada == "RSA":
     rsa_app()
 elif opcao_selecionada == "Hash":
     hash_app()
-elif opcao_selecionada == "Nuvem":
-    nuvem_app()
 
 st.sidebar.markdown("---")
 st.sidebar.info("Escolha uma cifra no menu acima para começar.")
